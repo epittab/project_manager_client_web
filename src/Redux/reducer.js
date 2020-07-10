cd21dd0ecc733fa4332022b219c8330b364dd934
epittab <ep7m@me.com>
2020-07-10 12:23:39 -0400

    included redux and created basic connections

---
+++ src/Redux/reducer.js
@@ -0,0 +1,15 @@
+
+const reducer = (oldState, action) => {
+
+    switch (action.type) {
+
+        case "TEST":
+            return oldState
+        default:
+            return oldState
+
+    }
+    
+}
+
+export default reducer;
