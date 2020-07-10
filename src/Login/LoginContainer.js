f425e0dc766099f54cb231ab8a098cb1f71e6bd1
epittab <ep7m@me.com>
2020-07-09 23:16:53 -0400

    began file directory structure for login and navbar

---
+++ src/Login/LoginContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './LoginContainer.css'
+
+class LoginContainer extends React.Component {
+    render(){
+        return (
+            <div className='LoginContainer'>
+
+            </div>
+        )
+    }
+}
+
+export default LoginContainer;
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Navbar.css'
+
+class Navbar extends React.Component {
+    render(){
+        return (
+            <nav className='Navbar'>
+
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
