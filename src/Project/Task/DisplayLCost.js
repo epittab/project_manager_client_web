ebe44cdb8faf7bc3b5ff74de745c13035dda32dd
epittab <ep7m@me.com>
2020-08-19 09:58:34 -0400

    fixed labor cost bug due to renaming field

--- src/Project/Task/DisplayLCost.js
+++ src/Project/Task/DisplayLCost.js
@@ -5,12 +5,12 @@ const DisplayLCost = ({cost}) => {
     return (
         <div className='line-item-wrapper'>
             <div className='col'>
-                <h3><span className='line-item'>Cost Name:</span> {cost.time_task_name}</h3>
-                <p><span className='line-item'>Cost Description:</span> {cost.time_task_description}</p>
+                <h3><span className='line-item'>Cost Name:</span> {cost.labor_name}</h3>
+                <p><span className='line-item'>Cost Description:</span> {cost.labor_description}</p>
 
             </div>
             <div className='col'>
-                <p><span className='line-item'>Cost:</span>{cost.user_hours}</p>
+                <p><span className='line-item'>Cost:</span>{cost.calculated_cost}</p>
 
             </div>
         </div>
